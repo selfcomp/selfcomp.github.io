04fc58b0d0a68d957414cb9b6b5b8f17ae4ac2ca
Renato Fabbri <renato.fabbri@gmail.com>
2018-06-03 20:36:26 -0300

    linhas jah aparecendo

--- selfcomp.py
+++ selfcomp.py
@@ -2,27 +2,55 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-works = [
-         ('GitHub', 'https://github.com/ttm'),
-         ('arXiv', 'https://arxiv.org/a/fabbri_r_1.html'),
-         ('Lattes', 'http://lattes.cnpq.br/1840472218825589'),
-         ('Sound Cloud', 'https://soundcloud.com/le-poste-tche/')
-        ]
-
-interests = ['data audiovisualization', 'linked data', 'complex networks', 'text mining', 'computational inteligence']
-
-selected = ['Fabbri, R., Fabbri, R., Antunes, D. C., Pisani, M. M., & de Oliveira Junior, O. N. (2017). Temporal stability in human interaction networks. Physica A: Statistical Mechanics and its Applications, 486, 92-105',
-            'Vieira, V., Fabbri, R., Travieso, G., Oliveira Jr, O. N., & da Fontoura Costa, L. (2012). A quantitative approach to evolution of music and philosophy. Journal of Statistical Mechanics: Theory and Experiment, 2012(08), P08010.',
-            'Fabbri, R. (2017). Topological stability and textual differentiation in human interaction networks: statistical analysis, visualization and linked data (Doctoral dissertation, Universidade de São Paulo).',
-            'Fabbri, R., Junior, V. V. D. S., Pessotti, A. C. S., Corrêa, D. C., & Oliveira Jr, O. N. (2014). Musical elements in the discrete-time representation of sound. arXiv preprint arXiv:1412.6853.',
-            'Half-Shape suite'
-           ]
+import os, re
+scwiki_path = '/home/renato/.vim/pack/prv/opt/wiki/aux/wiki/selfcomp/'
+files = os.listdir(scwiki_path)
+files = [i for i in files if 'html' not in i]
+df = {}
+for f in files:
+    f_ = scwiki_path + f
+    if os.path.isfile(f_):
+        ff = open(f_).readlines()
+        ff = [i.replace("\n", '') for i in ff]
+        ff = [re.sub(r"(./[a-zA-Z/]*)", r'<a href="\1">\1</a>', i) for i in ff]
+        df[f]=ff
 
 
 @app.route("/")
-def hello():
-    return render_template('child.html', title='ttm home', works=works, interests=interests, selected=selected)
+def index():
+    return render_template('index.html', title='selfcomp, auto-compilations directory', lines=df['index'])
 
+# @app.route("/about/")
+# def index():
+#     return render_template('about.html')
+# 
+# @app.route("/notes/")
+# def index():
+#     return render_template('notes.html')
+# 
+# @app.route("/readme/")
+# def index():
+#     return render_template('readme.html')
+# 
+# @app.route("/collections/")
+# def index():
+#     return render_template('collections.html')
+# 
+# @app.route("/screencast/")
+# def index():
+#     return render_template('screencast.html')
+# 
+# @app.route("/releases/")
+# def index():
+#     return render_template('releases.html')
+# 
+# @app.route("/rec/<person>")
+# def rec():
+#     return render_template('rec.html')
+# 
+# @app.route("/rel/<int:release>")
+# def rel():
+#     return render_template('rel.html')
 
 
 
